97b25c8c16788080d805a7f459461d94c69bd340
ToshBrown <Toshbrown@gmail.com>
2017-09-07 15:50:54 +0100

    It should be a json encode string that is escaped! quotes not needed

--- export.go
+++ export.go
@@ -7,7 +7,9 @@ var exportServiceURL = os.Getenv("DATABOX_EXPORT_SERVICE_ENDPOINT")
 
 func ExportLongpoll(destination string, payload string) (string, error) {
 
-	var jsonStr = "{\"id\":\"\",\"uri\":\"" + destination + "\",\"data\":\"" + payload + "\"}"
+	//TODO patload must be an escaped json string detect it it is not and error or escape it!!
+
+	var jsonStr = `{\"id\":\"\",\"uri\":\"" + destination + "\",\"data\":" + payload + "}`
 
 	fmt.Println("Sending ", jsonStr)
 
