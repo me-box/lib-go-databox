5ef5b92c7cadba475d35709bf5689e09bcd7b7d8
Chris Greenhalgh <chris.greenhalgh@nottingham.ac.uk>
2017-10-27 16:46:11 +0100

    adds StoreJSONGetrange and StoreJSONGetsince

--- store-json.go
+++ store-json.go
@@ -1,8 +1,41 @@
 package libDatabox
 
+import (
+	"strconv"
+)
+
 func StoreJSONGetlatest(href string) (string, error) {
 
 	data, err := makeStoreRequest(href+"/ts/latest", "GET")
+	if err != nil {
+		return "", err
+	}
+
+	return data, nil
+
+}
+
+func StoreJSONGetrange(href string, startTimestamp int64, endTimestamp int64) (string, error) {
+
+	params :=  map[string]string {
+		"startTimestamp": strconv.FormatInt(startTimestamp, 10),
+		"endTimestamp": strconv.FormatInt(endTimestamp, 10),
+	}
+	data, err := makeStoreRequestForm(href+"/ts/range", "GET",  params)
+	if err != nil {
+		return "", err
+	}
+
+	return data, nil
+
+}
+
+func StoreJSONGetsince(href string, startTimestamp int64) (string, error) {
+
+	params :=  map[string]string {
+		"startTimestamp": strconv.FormatInt(startTimestamp, 10),
+	}
+	data, err := makeStoreRequestForm(href+"/ts/since", "GET",  params)
 	if err != nil {
 		return "", err
 	}
