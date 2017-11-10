acc3fef33fab32421b3ad5448541d162fcf05f8f
ToshBrown <Toshbrown@gmail.com>
2017-11-10 10:41:21 +0000

    Update sample app and driver

--- samples/app-hello-world/src/main.go
+++ samples/app-hello-world/src/main.go
@@ -6,28 +6,30 @@ import (
 	"net/http"
 	"os"
 
-	databox "github.com/me-box/lib-go-databox"
+	//databox "github.com/me-box/lib-go-databox"
+	databox "github.com/toshbrown/lib-go-databox"
 )
 
-var dataSourceTest, _ = databox.JsonUnmarshal(os.Getenv("DATASOURCE_test"))
-var storeURL, _ = databox.GetStoreURLFromDsHref(dataSourceTest["href"].(string))
+var dataSourceTest, _ = databox.HypercatToDataSourceMetadata(os.Getenv("DATASOURCE_test"))
+var storeEndPoint = os.Getenv("DATABOX_ZMQ_ENDPOINT")
 
 func main() {
 
-	fmt.Printf(storeURL + "\n")
-
-	databox.WaitForStoreStatus(storeURL)
+	tsClient, err := databox.NewJSONTimeSeriesClient(storeEndPoint, false)
+	if err != nil {
+		panic("Can't connect to databox store at " + storeEndPoint)
+	}
 
 	//start the https server for the app UI
 	http.HandleFunc("/ui", func(w http.ResponseWriter, r *http.Request) {
 
-		data, err := databox.StoreJSONGetlatest(dataSourceTest["href"].(string))
+		data, err := tsClient.Latest(dataSourceTest.DataSourceID)
 
 		if err != nil {
 			fmt.Fprintf(w, "<html><body><h1>hello world! from a databox app</h1><p>error:: "+err.Error()+"</p></body></html>\n")
 		}
 
-		fmt.Fprintf(w, "<html><body><h1>hello world! from a databox app</h1><p>Latest from the driver"+data+"</p></body></html>\n")
+		fmt.Fprintf(w, "<html><body><h1>hello world! from a databox app</h1><p>Latest from the driver"+string(data)+"</p></body></html>\n")
 	})
 	//The https server is setup to offer the configuration UI for your app
 	//you can use any framework you like to display the interface and parse
